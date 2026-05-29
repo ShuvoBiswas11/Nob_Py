36daf8bd1f6ddc9c02437e7da957ba7be12d084c
ShuvoBiswas11 <biswasshuvo285@gmail.com>
2026-05-29 22:09:30 +0600

    Added python code for finding largest number between two

---
+++ largest_of_two_numbers.py
@@ -0,0 +1,11 @@
+#Getting Inputs from user
+
+num1 = int(input("Enter the first number : "))
+num2 = int(input("Enter the second number : "))
+
+#if condition
+
+if num1>num2:
+    print(f"{num1} is the largest number.")
+else:
+    print(f"{num2} is the largest number.")
