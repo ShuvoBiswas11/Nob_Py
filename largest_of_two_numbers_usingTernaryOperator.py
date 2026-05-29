8fb3385c8157bd35cdb529e1553d6630e8d693bb
ShuvoBiswas11 <biswasshuvo285@gmail.com>
2026-05-29 22:40:16 +0600

    Added largest_of_two_numbers_usingTernaryOperator.py

---
+++ largest_of_two_numbers_usingTernaryOperator.py
@@ -0,0 +1,10 @@
+#Getting inputs from user
+
+num1 = int (input ("Enter first number : "))
+num2 = int (input ("Enter second number : "))
+
+#condition
+
+large = num1 if num1>num2 else num2
+
+print("The largest number is : " ,large)
