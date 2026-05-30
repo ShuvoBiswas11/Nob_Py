453c8e0a1231bfb3141c88c43ab126e47c05b729
ShuvoBiswas11 <biswasshuvo285@gmail.com>
2026-05-30 13:05:45 +0600

    Added vowel_or_consonant.py

---
+++ vowel_or_consonant.py
@@ -0,0 +1,10 @@
+#Getting input from user
+
+ch = input ("Enter a letter : ")
+
+#Condition
+
+if ch == "a" or ch == "e" or ch == "i" or ch == "o" or ch == "u" or ch == "A" or ch == "E" or ch == "I" or ch == "O" or ch == "U":
+    print (f"{ch} is vowel")
+else:
+    print (f"{ch} is consonant")
