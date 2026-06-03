542619043b4d235435fc3c9dcbfe744dc36e8d2c
ShuvoBiswas11 <biswasshuvo285@gmail.com>
2026-06-03 21:55:49 +0600

    Added list_range_practice.py

---
+++ list_range_practice.py
@@ -0,0 +1,15 @@
+#Initialize Value in List using range
+
+print("\nPrint a list contains 0-14 using range :")
+number = list(range(15))                #Initilize 0-14 as list in number variable
+print(number)
+
+
+print("\nPrint 11 to 20 as a list using range :")
+number2 = list(range(11,21))            #range(11,21) : 11 = start point and 21 = end point(It will store value from 11 to 20)
+print(number2)
+
+
+print("\nPrint only odd numbers from 1 to 10 as a list using range :")
+odd = list(range(1,11,2))
+print(odd)
