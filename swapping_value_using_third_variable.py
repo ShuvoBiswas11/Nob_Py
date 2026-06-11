2c8bc2e9c4119c2dce3888e4a56048e7bfd267ac
ShuvoBiswas11 <biswasshuvo285@gmail.com>
2026-06-11 17:42:27 +0600

    Update swapping_value_using_third_variable.py

--- swapping_value_using_third_variable.py
+++ swapping_value_using_third_variable.py
@@ -1,7 +1,7 @@
 # Getting value from user
 
-num1 = int(input("Enter the value for num1 : "))
-num2 = int(input("Enter the value for num2 : "))
+num1 = int(input("Enter an integer value for num1 : "))
+num2 = int(input("Enter an integer value for num2 : "))
 temp = 0                                                        # initialize 0 into temp variable
 
 print (f"Before swapping num1 = {num1} and num2 = {num2}")
